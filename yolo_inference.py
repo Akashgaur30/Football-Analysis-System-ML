24e4983f302f0b43433b5cd1852efa9c7294927d
Akash Gaur <91282551+Akashgaur30@users.noreply.github.com>
2024-09-06 22:01:21 +0530

    after yolo apply

---
+++ yolo_inference.py
@@ -0,0 +1,13 @@
+
+from ultralytics import YOLO
+
+
+model = YOLO('yolov8s')
+
+results = model.predict('input_videos/foo.mp4', save=True)
+
+print(results[0])
+print('=========================')
+
+for box in results[0].boxes:
+    print(box)
